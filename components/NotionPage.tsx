61e617b3246acd1198f06bc132b20ea97f0600fd
leeyoonsu <dbstndla1212@naver.com>
2023-02-27 21:14:24 +0900

    [REFACTOR] Primsjs 추가하여 코드블럭 하이라이팅 Todo : NotionPage.tsx에 @ts-nocheck 부분 확인하여 수정시 지우기

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck : prismjs의 타입 체크에 문제가 있어 타입 지정이 되지 않음
+// todo : 타입 체크 고쳐지면 지우기
 import * as React from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
@@ -24,7 +26,42 @@ export const NotionPage = ({
   }
 
   const Code = dynamic(() =>
-    import("react-notion-x/build/third-party/code").then((m) => m.Code)
+    import("react-notion-x/build/third-party/code").then(async (m) => {
+      await Promise.all([
+        import("prismjs/components/prism-markup-templating.js"),
+        import("prismjs/components/prism-markup.js"),
+        import("prismjs/components/prism-bash.js"),
+        import("prismjs/components/prism-c.js"),
+        import("prismjs/components/prism-cpp.js"),
+        import("prismjs/components/prism-csharp.js"),
+        import("prismjs/components/prism-docker.js"),
+        import("prismjs/components/prism-java.js"),
+        import("prismjs/components/prism-js-templates.js"),
+        import("prismjs/components/prism-coffeescript.js"),
+        import("prismjs/components/prism-diff.js"),
+        import("prismjs/components/prism-git.js"),
+        import("prismjs/components/prism-go.js"),
+        import("prismjs/components/prism-graphql.js"),
+        import("prismjs/components/prism-handlebars.js"),
+        import("prismjs/components/prism-less.js"),
+        import("prismjs/components/prism-makefile.js"),
+        import("prismjs/components/prism-markdown.js"),
+        import("prismjs/components/prism-objectivec.js"),
+        import("prismjs/components/prism-ocaml.js"),
+        import("prismjs/components/prism-python.js"),
+        import("prismjs/components/prism-reason.js"),
+        import("prismjs/components/prism-rust.js"),
+        import("prismjs/components/prism-sass.js"),
+        import("prismjs/components/prism-scss.js"),
+        import("prismjs/components/prism-solidity.js"),
+        import("prismjs/components/prism-sql.js"),
+        import("prismjs/components/prism-stylus.js"),
+        import("prismjs/components/prism-swift.js"),
+        import("prismjs/components/prism-wasm.js"),
+        import("prismjs/components/prism-yaml.js"),
+      ]);
+      return m.Code;
+    })
   );
   const Collection = dynamic(() =>
     import("react-notion-x/build/third-party/collection").then(
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "react-notion-x/src/styles.css"; // react-notion-x core styles
+import "prismjs/themes/prism-tomorrow.css"; // prism code syntax highlighting
 import "katex/dist/katex.min.css"; // react-notion-x equations
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return <Component {...pageProps} />;
 }
 
