066fe05d4867a95d877e6150059660b2f1116011
leeyoonsu <dbstndla1212@naver.com>
2023-02-28 22:16:06 +0900

    [REFACTOR] 기존 html 조정 개선 기존에 a태그를 div태그로 모두 바꿔줬으나 a태그의 기존 동작을 막고 해당 href로 router.push 해주어 새로고침 발생X

--- components/Memo.tsx
+++ components/Memo.tsx
@@ -23,7 +23,6 @@ const Memo = (props: any) => {
   );
 };
 
-
 // Page를 통해 NotionPage가 렌더링되는데 , 최종적으로 해당 컴포넌트에서 html를 조회하여
 // 모든 a태그를 div태그로 변경하고 onclick 이벤트로 교체합니다.
 // Link from next/link와 같은 동작을 하도록 의도되었습니다.
@@ -49,15 +48,17 @@ export default function WrappedComponent(props: any) {
     };
   }, [router]);
 
+  // 기존 a태그의 동작을 막고 , 화면 새로고침 없이 렌더링되게 합니다.
   function pushPage(e: any) {
+    e.preventDefault();
     let id;
-    if (e.target.id) {
-      id = e.target.id;
+    if (e.target.href) {
+      id = e.target.href;
     } else {
-      //가장 가까운 div의 id에 페이지의 url이 담겨있습니다.
-      const div = e.target.closest("div");
-      if (div) {
-        id = div.id;
+      //가장 가까운 a 태그의 id에 페이지의 url이 담겨있습니다.
+      const aTag = e.target.closest("a");
+      if (aTag) {
+        id = aTag.href;
       }
       // 페이지를 클릭하면 페이지 조정을 하기 전까지 가립니다.
     }
@@ -82,16 +83,17 @@ export default function WrappedComponent(props: any) {
       // Todo: react-notion-x 라이브러리가 개선되면 수정하기
       for (let i = 0; i < aTags.length; i++) {
         const aTag = aTags[i];
-        const href = aTag.getAttribute("href");
-        const div = document.createElement("div");
-        div.className = aTag.className + " hover:cursor-pointer";
-        div.onclick = pushPage;
-        div.id = href!;
-        while (aTag.firstChild) {
-          div.appendChild(aTag.firstChild);
-        }
-        aTag.parentNode?.insertBefore(div, aTag);
-        aTag.remove();
+        aTag.onclick = pushPage;
+        // const href = aTag.getAttribute("href");
+        // const div = document.createElement("div");
+        // div.className = aTag.className + " hover:cursor-pointer";
+        // div.onclick = pushPage;
+        // div.id = href!;
+        // while (aTag.firstChild) {
+        //   div.appendChild(aTag.firstChild);
+        // }
+        // aTag.parentNode?.insertBefore(div, aTag);
+        // aTag.remove();
       }
 
       const contentElement = document.getElementById("content");
